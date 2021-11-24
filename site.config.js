c16c0e225b0b5f4c4dda866acb40b2d198094d07
renfeiyang601 <62423479+renfeiyang601@users.noreply.github.com>
2021-11-24 22:21:40 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '1595c2e6e8b345afa1e74b70633eb037',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '云飞扬工作室',
+  domain: 'https://www.notion.so/Getting-Started-on-Mobile-1595c2e6e8b345afa1e74b70633eb037',
+  author: 'YUKI',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,9 +17,8 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '@XEtUGCxWQwJGVDC',
+  github: 'renfeiyang601',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
